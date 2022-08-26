a75ea08be642c14489a066f924f3b9024b61d951
Matias2388 <106512472+Matias2388@users.noreply.github.com>
2022-08-26 16:03:18 +0200

    llamando a conulta cambio

--- Criptocambio/models.py
+++ Criptocambio/models.py
@@ -1,6 +1,14 @@
+import datetime
+import random
+import sqlite3
+from typing import List
+
 import requests
-#from .import APIKEY
+
+# from .import APIKEY
 APIKEY = "36EAEB03-5E48-4A18-9C93-1F2F16B9B9E5"
+
+
 class APIError(Exception):
     pass
 
@@ -12,7 +20,8 @@ class CriptoModel:
     - cambio
     - consultar cambio (método)
     """
-    def __init__ (self,origen,destino):
+
+    def __init__(self, origen, destino):
         """
         Construye un objeto con las monedas origen y destino
         y el cambio obtenido desde CoinAPI inicializado a cero.
@@ -26,9 +35,14 @@ class CriptoModel:
         Consulta el cambio entre la moneda origen y la moneda destino
         utilizando la API REST CoinAPI.
         """
+
+        self.cambio = random.random()
+        return
+
         cabeceras = {
             "X-CoinAPI-Key": APIKEY
         }
+
         url = f"http://rest.coinapi.io/v1/exchangerate/{self.moneda_origen}/{self.moneda_destino}"
         respuesta = requests.get(url, headers=cabeceras)
 
@@ -42,3 +56,41 @@ class CriptoModel:
                 )
             )
 
+
+class Transaccion:
+    def __init__(self, origen, destino, cantidad):
+        self.origen =  origen
+        self.destino = destino
+        self.cantidad = cantidad
+        self.fecha = datetime.datetime.now()
+
+class Database:
+    def __init__(self):
+        self.db = sqlite3.connect("database.db", check_same_thread=False)
+        self.db.execute("""CREATE TABLE IF NOT EXISTS transacciones (
+            id integer PRIMARY KEY,
+            origen text,
+            destino text,
+            cantidad integer,
+            fecha text
+        );""")
+
+    def guardar_transaccion(self, tx: Transaccion):
+        cur = self.db.cursor()
+        cur.execute("INSERT INTO transacciones(origen, destino, cantidad, fecha) VALUES (?, ?, ?, ?)",
+                    (tx.origen, tx.destino, tx.cantidad, tx.fecha.timestamp()))
+
+        self.db.commit()
+
+    def conseguir_transacciones(self) -> List[Transaccion]:
+        cur = self.db.cursor()
+        cur.execute("SELECT * FROM transacciones")
+
+        data = []
+        for fila in cur.fetchall():
+            tx = Transaccion(fila[1], fila[2], fila[3])
+            tx.fecha = datetime.datetime.fromtimestamp(float(fila[4]))
+
+            data.append(tx)
+
+        return data
--- Criptocambio/views.py
+++ Criptocambio/views.py
@@ -1,6 +1,12 @@
-from flask import Flask,render_template,request,abort
-from . import app
-from.models import CriptoModel
+from models import CriptoModel, Database, Transaccion
+
+APIKEY = "36EAEB03-5E48-4A18-9C93-1F2F16B9B9E5"
+
+from flask import Flask, render_template, request, abort
+
+db = Database()
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -9,11 +15,14 @@ def home():
     """
     return render_template("nuevo.html")
 
-@app.route('/compra',methods=['GET', 'POST'])
-def compra():
-    if request.method == "GET":
-        return render_template("index.html")
 
+@app.get('/consulta')
+def consulta_inicio():
+    return render_template("index.html")
+
+
+@app.post('/consulta')
+def consulta_resultado():
     origen = request.form.get("origen")
     destino = request.form.get("destino")
 
@@ -22,9 +31,30 @@ def compra():
 
     crypto = CriptoModel(origen, destino)
     crypto.consultar_cambio()
-    
-    return render_template("index.html", crypto=crypto.cambio)
 
+    cambio = round(crypto.cambio, 5)
+    return render_template("index.html", crypto=cambio)
+
+
+@app.post('/compra')
+def compra():
+    origen = request.form.get("origen")
+    destino = request.form.get("destino")
+    cantidad = request.form.get("cantidad")
+
+    db.guardar_transaccion(Transaccion(origen, destino, cantidad))
+
+    return f"{origen} {destino} {cantidad}"
+
+@app.post('/transacciones')
+def transacciones():
+    transacciones = db.conseguir_transacciones()
+
+    s = ""
+    for tx in transacciones:
+        s += f"{tx.origen} {tx.destino} {tx.cantidad} {tx.fecha}\n"
+
+    return s
 
 @app.route('/estado')
 def actualizar():
